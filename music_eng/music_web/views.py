5c75b2c88d33328882cb1a57e784c9f1ee63db80
elena <elenaliviapopa21@gmail.com>
2023-04-05 18:38:11 +0200

    views

--- music_eng/music_web/views.py
+++ music_eng/music_web/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from web.models import Song, Playlist, Album, Author, DatePublication, MusicalGenre
+from django.views import generic
 
-# Create your views here.
+def home(request):
+        songs = Song.objects.all()
+        playlists = Playlist.objects.all()
+        albums = Album.objects.all()
+        authors = Author.objects.all()
+        musicalgenres = MusicalGenre.objects.all()
+        return render(request, "web/index.html", {'songs': songs, 'playlists': playlists, 'albums': albums, 'authors': authors, 'musicalgenres': musicalgenres})
+
+def song(request, song_id):
+        try:
+            song = Song.objects.get(pk=song_id)
+        except Song.DoesNotExist:
+                raise Http404("Aquesta canço no existeix")
+        return render(request, 'web/song.html', {'song': song})
+
+class DatePublicationView(generic.DetailView):
+       model = DatePublication
+       template_name = 'web/datepublication.html'
