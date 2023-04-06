10f6a9e717a74d8583a8c29eb128890efc3213ff
vGerJ02 <gjaimejuanguardia@gmail.com>
2023-04-06 18:30:54 +0200

    Divided settings in development and production, added files .env, requirements.txt and static/

---
+++ music_eng/music_eng/settings/production.py
@@ -0,0 +1,29 @@
+from .base import *
+import environ
+import os
+
+env = environ.Env(
+    DEBUG=(bool, False),
+    ALLOWED_HOSTS=(list, [])
+)
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env('DEBUG')
+
+ALLOWED_HOSTS: list[str] = env('ALLOWED_HOSTS')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASS'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+
+    }
+}
